c1881c2b2bd799fe0b9d5cbe574f37bf39058667
Douglas McCloskey <dmccloskey87@gmail.com>
2019-04-21 21:29:23 +0200

    WIP: testing different lamda/tuple iterator methods

--- src/tensorbase/include/TensorBase/ml/TensorCollection.h
+++ src/tensorbase/include/TensorBase/ml/TensorCollection.h
@@ -8,26 +8,39 @@
 
 namespace TensorBase
 {
-
   /**
     @brief Class for managing heterogenous Tensors
   */
+  template<class... TTables>
   class TensorCollection
   {
   public:
     TensorCollection() = default;  ///< Default constructor
+    TensorCollection(TTables&... tTables) { 
+      tables_ = std::make_tuple(tTables...); 
+    };  ///< Default constructor
     ~TensorCollection() = default; ///< Default destructor
 
-    void setId(const int& id) { id_ = id; }; ///< id setter
-    int getId() const { return id_; }; ///< id getter
+    void setTableNames(const std::vector<std::string>& names) { 
+      tables_to_indices_.clear();
+      for (const std::string&name : names)
+        tables_to_indices_.emplace(name, tables_to_indices_.size());
+    }; ///< table names setter
+    std::vector<std::string> getTableNames() const { 
+      std::vector<std::string> names;
+      for (const auto& table_to_index_map : tables_to_indices_)
+        names.push_back(table_to_index_map.first);
+      return names; 
+    }; ///< table neames getter
 
-    void setName(const std::string& name) { name_ = name; }; ///< name setter
-    std::string getName() const { return name_; }; ///< name getter
+    int getTableIndex(const std::string& name);
 
+    auto& getTable(const::std::string& name);  ///< non-const TensorTable getter
+    const auto& getTable(const::std::string& name) const;  ///< const TensorTable getter
 
   private:
-    int id_ = -1;
-    std::string name_ = "";
+    std::map<std::string, int> tables_to_indices_;
+    std::tuple<std::shared_ptr<TTables>...> tables_; ///< tuple of TensorTables<TensorT, DeviceT, TDim>
     
     //private:
     //	friend class cereal::access;
@@ -36,5 +49,26 @@ namespace TensorBase
     //		archive(id_, name_, n_dimensions_, n_labels_, tensor_dimension_names_, tensor_dimension_labels_);
     //	}
   };
+  template<class ...TTables>
+  inline int TensorCollection<TTables...>::getTableIndex(const std::string & name)
+  {
+    if (tables_to_indices_.count(name))
+      return tables_to_indices_.at(name);
+    else
+      return -1;
+  }
+  template<class ...TTables>
+  inline auto& TensorCollection<TTables...>::getTable(const std::string & name)
+  {
+
+  }
+  template<class ...TTables>
+  inline const auto& TensorCollection<TTables...>::getTable(const std::string & name) const
+  { // Duplicate of the non-const getter...
+    if (tables_to_indices_.count(name)) {
+      int index = tables_to_indices_.at(name);
+      return std::get<index>(tables_);
+    }
+  }
 };
 #endif //TENSORBASE_TENSORCOLLECTION_H
--- src/tests/class_tests/tensorbase/source/TensorCollection_test.cpp
+++ src/tests/class_tests/tensorbase/source/TensorCollection_test.cpp
@@ -11,32 +11,135 @@ BOOST_AUTO_TEST_SUITE(tensorCollection)
 
 BOOST_AUTO_TEST_CASE(constructor) 
 {
-  TensorCollection* ptr = nullptr;
-  TensorCollection* nullPointer = nullptr;
-	ptr = new TensorCollection();
+  TensorCollection<TensorTable<float,Eigen::DefaultDevice,3>>* ptr = nullptr;
+  TensorCollection<TensorTable<float, Eigen::DefaultDevice, 3>>* nullPointer = nullptr;
+	ptr = new TensorCollection<TensorTable<float, Eigen::DefaultDevice, 3>>();
   BOOST_CHECK_NE(ptr, nullPointer);
   delete ptr;
 }
 
 BOOST_AUTO_TEST_CASE(destructor)
 {
-  TensorCollection* ptr = nullptr;
-	ptr = new TensorCollection();
+  TensorCollection<TensorTable<float, Eigen::DefaultDevice, 3>>* ptr = nullptr;
+	ptr = new TensorCollection<TensorTable<float, Eigen::DefaultDevice, 3>>();
   delete ptr;
 }
 
+// Implementation 1a
+template<typename TupleType, typename FunctionType>
+void for_each(TupleType&&, FunctionType, std::integral_constant<size_t, std::tuple_size<typename std::remove_reference<TupleType>::type >::value>) {}
+
+template<std::size_t I, typename TupleType, typename FunctionType, 
+  typename = typename std::enable_if<I != std::tuple_size<typename std::remove_reference<TupleType>::type>::value>::type >
+  void for_each(TupleType&& t, FunctionType f, std::integral_constant<size_t, I>)
+{
+  f(std::get<I>(t));
+  for_each(std::forward<TupleType>(t), f, std::integral_constant<size_t, I + 1>());
+}
+
+template<typename TupleType, typename FunctionType>
+void for_each(TupleType&& t, FunctionType f)
+{
+  for_each(std::forward<TupleType>(t), f, std::integral_constant<size_t, 0>());
+}
+
+// Implementation 1b
+template<typename TupleType, typename FunctionType>
+constexpr auto for_each_get(TupleType&&, FunctionType, std::integral_constant<size_t, std::tuple_size<typename std::remove_reference<TupleType>::type >::value>) {}
+
+template<std::size_t I, typename TupleType, typename FunctionType,
+  typename = typename std::enable_if<I != std::tuple_size<typename std::remove_reference<TupleType>::type>::value>::type >
+constexpr auto for_each_get(TupleType&& t, FunctionType f, std::integral_constant<size_t, I>)
+{
+  f(std::get<I>(t));
+  return for_each_get(std::forward<TupleType>(t), f, std::integral_constant<size_t, I + 1>());
+}
+
+template<typename TupleType, typename FunctionType>
+constexpr auto for_each_get(TupleType&& t, FunctionType f)
+{
+  return for_each_get(std::forward<TupleType>(t), f, std::integral_constant<size_t, 0>());
+}
+
+// Implementation 2a
+template <class F, size_t... Is>
+constexpr auto apply_impl(F f, std::index_sequence<Is...>) {
+  return f(std::integral_constant<size_t, Is> {}...);
+}
+
+template <class Tuple, class F>
+constexpr auto apply(Tuple t, F f) {
+  return apply_impl(f, std::make_index_sequence<std::tuple_size<Tuple>{}> {});
+}
+
+// Implementation 2b
+template <class F, size_t... Is>
+constexpr auto index_apply_impl(F f, std::index_sequence<Is...>) {
+  return f(std::integral_constant<size_t, Is> {}...);
+}
+
+template <size_t N, class F>
+constexpr auto index_apply(F f) {
+  return index_apply_impl(f, std::make_index_sequence<N>{});
+}
+
+template <size_t N, class Tuple>
+constexpr auto take_front(Tuple t) {
+  return index_apply<N>([&](auto... Is) {
+    return std::make_tuple(std::get<Is>(t)...);
+  });
+}
+
+template <typename T>
+struct capture_element{
+  void operator()(const T& t) {
+    template<typename std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
+    if (t == comp) element = t;
+  }
+  T element;
+  T comp;
+};
+
 BOOST_AUTO_TEST_CASE(gettersAndSetters)
 {
-  TensorCollection tensorCollection;
-  // Check defaults
-  BOOST_CHECK_EQUAL(tensorCollection.getId(), -1);
-  BOOST_CHECK_EQUAL(tensorCollection.getName(), "");
+  TensorCollection<
+    TensorTable<float, Eigen::DefaultDevice, 3>,
+    TensorTable<int, Eigen::DefaultDevice, 2>,
+    TensorTable<char, Eigen::DefaultDevice, 4>
+  > tensorCollection;
 
-  // Check getters/setters
-  tensorCollection.setId(1);
-  tensorCollection.setName("1");
-  BOOST_CHECK_EQUAL(tensorCollection.getId(), 1);
-  BOOST_CHECK_EQUAL(tensorCollection.getName(), "1");
+  // name setters and getters
+  tensorCollection.setTableNames({"tfloat", "tint", "tchar"});
+  BOOST_CHECK(tensorCollection.getTableNames() == std::vector<std::string>({ "tfloat", "tint", "tchar" }));
+
+  // table getters
+  //BOOST_CHECK_EQUAL(tensorCollection.getTableIndex("tfloat"), 0);
+  //BOOST_CHECK_EQUAL(tensorCollection.getTable("tfloat")->getName(), "tfloat");
+  //BOOST_CHECK_EQUAL(tensorCollection.getTable("tint")->getName(), "tint");
+  //BOOST_CHECK_EQUAL(tensorCollection.getTable("tchar")->getName(), "tchar");
+}
+
+BOOST_AUTO_TEST_CASE(Scratch)
+{
+  auto some = std::make_tuple("I am good", 255, 2.1);
+  for_each(some, [](const auto &x) { std::cout << x << std::endl; });
+
+  auto t = take_front<2>(std::make_tuple("I am good", 255, 2.1));
+  for_each(t, [](const auto &x) { std::cout << x << std::endl; });
+
+  auto x = apply(std::make_tuple(1, 2), plus<>{});
+  std::cout << x << std::endl;
+
+  //float y=0;
+  //for_each_get(std::make_tuple("I am good", 255, 2.1), [&y](const auto& x) {
+  //  if (x == 3.3) y=x; 
+  //});
+  //std::cout << y << std::endl;
+
+  capture_element<float> ce;
+  ce.comp = 2.1;
+  for_each_get(std::make_tuple("I am good", 255, 2.1), ce);
+  std::cout << ce.element << std::endl;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
