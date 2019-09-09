3f3bd5fe4e101a699091cdd609016151b9d491f0
Douglas McCloskey <dmccloskey87@gmail.com>
2019-09-09 15:19:18 +0200

    WIP: adding in method templates to TensorTableFile

--- src/tensorbase/include/TensorBase/io/TensorTableFile.h
+++ src/tensorbase/include/TensorBase/io/TensorTableFile.h
@@ -55,7 +55,36 @@ public:
     std::string makeTensorTableShardFilename(const std::string& dir, const std::string& tensor_table_name, const int& shard_id);
 
     template<typename DeviceT>
-    std::vector<int> getModifiedShardIDs(const std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& is_modified, )
+    std::vector<int> getModifiedShardIDs(
+      const std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& is_modified,
+      const std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& shard_id,
+      const std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& shard_indices,
+      std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& modified_shard_id,
+      std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& modified_shard_indices);
+
+    template<typename DeviceT, int TDim>
+    std::vector<int> makeSliceIndicesFromShardIndices(
+      const std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& modified_shard_id,
+      const std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& modified_shard_indices,
+      std::map<int, std::pair<Eigen::array<int, TDim>, Eigen::array<int, TDim>>>& slice_indices);
+
+    template<typename TensorT, typename DeviceT, int TDim>
+    bool storeTensorTableShard(const std::string& filename,
+      const Eigen::Tensor<TensorT, TDim>& tensor_data,
+      const std::pair<Eigen::array<int, TDim>, Eigen::array<int, TDim>>& slice_indices);
+
+    template<typename DeviceT>
+    std::vector<int> getNotInMemoryShardIDs(
+      const std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& is_modified,
+      const std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& shard_id,
+      const std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& shard_indices,
+      std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& modified_shard_id,
+      std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>>& modified_shard_indices);
+
+    template<typename TensorT, typename DeviceT, int TDim>
+    bool loadTensorTableShard(const std::string& filename,
+      Eigen::Tensor<TensorT, TDim>& tensor_data,
+      const std::pair<Eigen::array<int, TDim>, Eigen::array<int, TDim>>& slice_indices);
   };
 
   template<typename TensorT, typename DeviceT, int TDim>
@@ -64,7 +93,7 @@ public:
 
     // read in the shards and update the TensorTable data asyncronously
 
-    // update the `IsInMemory` tensor table attribute
+    // update the `in_memory` tensor table attribute
 
     return true
   }
@@ -72,8 +101,18 @@ public:
   template<typename TensorT, typename DeviceT, int TDim>
   inline bool TensorTableFile::storeTensorTableBinary(const std::string& dir, const TensorTable<TensorT, DeviceT, TDim>& tensor_table) {
     // determine the shards to write to disk
+    std::map<std::string, std::shared_ptr<TensorData<int, DeviceT, 1>>> modified_shard_id, modified_shard_indices;
+    getModifiedShardIDs(tensor_table.getIsModified(), tensor_table.getShardId(), tensor_table.getShardIndices(), modified_shard_id, modified_shard_indices);
+    std::map<int, std::pair<Eigen::array<int, TDim>, Eigen::array<int, TDim>>> slice_indices;
+    makeSliceIndicesFromShardIndices(modified_shard_id, modified_shard_indices, slice_indices);
 
     // write the TensorTable shards to disk asyncronously
+    for (const auto slice_index : slice_indices) {
+      const std::string filename = makeTensorTableShardFilename(dir, tensor_table.getName(), slice_index.first);
+      storeTensorTableShard(filename, tensor_table.getData(), slice_index.second)
+    }
+
+    // update the `is_modified` tensor table attribute
 
     return true;
   }
