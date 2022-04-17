a33b80604509b5cbbab14a2ee2d6eeac55150ee3
dmccloskey <dmccloskey87@gmail.com>
2022-04-17 12:26:10 +0200

    fix: updated third party location for eigen

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -14,7 +14,7 @@ message (STATUS "Adding Eigen ${EIGEN_VERSION} as an external project.")
 #endif ()
 
 ExternalProject_Add(eigen
-  GIT_REPOSITORY "https://github.com/eigenteam/eigen-git-mirror.git"
+  GIT_REPOSITORY "https://gitlab.com/libeigen/eigen.git"
   #GIT_TAG ${EIGEN_VERSION} # Need the dev branch to compile use MSVC
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
